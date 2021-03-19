4384cb44fbd2244d4bc307f108f2417660c1b037
Marc Nopprapun <mnopprapun@gmail.com>
2021-03-19 15:24:36 -0500

    completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id ='victory';
+
+newHeader.innerHTML = 'Marc is the champion';
+document.body.append(newHeader);
